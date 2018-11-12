2919df392c4713dbaf798fa29b800137d45f9372
kokovych <kokovych@gmail.com>
2018-11-12 23:31:58 +0200

    add prototype of login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import {User} from '../_models/user';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,8 +11,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   constructor(
     ) { }
-
+  user: User = new User();
   ngOnInit() {
+  }
+  onSubmit() {
+    console.log(this.user);
   }
 
 }
